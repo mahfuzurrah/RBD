a866bfdcef9a0c24ef73cbe4f514d16afbdff69d
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-04-17 10:26:16 +0600

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,27 @@
+document.getElementById("toggleButton").addEventListener("click", function () {
+    var content = document.querySelector(".text__content");
+    content.classList.toggle("showLess");
+
+    var buttonText = this.innerHTML === "Read More" ? "Read Less" : "Read More";
+    this.innerHTML = buttonText;
+});
+
+document.getElementById("toggleButton2").addEventListener("click", function () {
+    var content = document.querySelector(".text__content2");
+    content.classList.toggle("showLess");
+
+    var buttonText = this.innerHTML === "Read More" ? "Read Less" : "Read More";
+    this.innerHTML = buttonText;
+});
+
+
+// Upload Image
+
+function upload(){
+    var imgcanvas = document.getElementById("canv1");
+    var fileinput = document.getElementById("finput");
+    var image = new SimpleImage(fileinput);
+    image.drawTo(imgcanvas);
+  }
+
+
