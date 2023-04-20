2e2b8cc0cdcb7543a49ac0c79bbcc0f875c3c282
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-04-21 01:15:57 +0600

    Add

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,21 @@
+function toggle() {
+    var content = document.getElementById("navbarNav");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+
+
+
 document.getElementById("toggleButton").addEventListener("click", function () {
     var content = document.querySelector(".text__content");
     content.classList.toggle("showLess");
